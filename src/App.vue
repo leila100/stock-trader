6e56fc7436bfcb42ca62cd9957617ccf4562215a
Leila <nb.leila10@gmail.com>
2020-04-08 17:43:45 -0500

    Add header - nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <template>
   <div class="container">
-    <router-view />
+    <Header />
+    <div class="row">
+      <div class="col-xs-12">
+        <router-view />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import Header from "./components/Header.vue";
+export default {
+  components: {
+    Header,
+  },
+};
 </script>
 
-<style></style>
+<style>
+body {
+  padding: 30px;
+}
+</style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,42 @@
 <template>
-  <h1>Header component</h1>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <router-link to="/" class="navbar-brand">Stock Trader</router-link>
+
+    <div class="collapse navbar-collapse">
+      <ul class="navbar-nav mr-auto">
+        <router-link to="/portfolio" active="active" tag="li" class="nav-item"
+          ><a class="nav-link" href="#">Porfolio</a></router-link
+        >
+        <router-link to="stocks" active="active" tag="li" class="nav-item"
+          ><a class="nav-link" href="#">Stocks</a></router-link
+        >
+      </ul>
+      <ul class="navbar-nav navbar-right">
+        <li class="nav-item"><a href="#" class="nav-link">End Day</a></li>
+        <li class="nav-item dropdown">
+          <a
+            class="nav-link dropdown-toggle"
+            href="#"
+            id="navbarDropdown"
+            role="button"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            Save & Load
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Save data</a>
+            <a class="dropdown-item" href="#">Load Data</a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style>
-</style>
+<style></style>

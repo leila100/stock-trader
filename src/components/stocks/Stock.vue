dfa929ae5b46fae87e83a124c5e6310e2a7134f5
Leila <nb.leila10@gmail.com>
2020-04-08 16:40:27 -0500

    Add basic components

---
+++ src/components/stocks/Stock.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Stocks - stock component</h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ src/components/stocks/Stocks.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Stocks page</h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>

d1c44584b1e4810de1f01d714403b62b564d40b5
Leila <nb.leila10@gmail.com>
2020-04-09 18:36:15 -0500

    Display funds in home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,25 @@
 <template>
-  <h1>Home page</h1>
+  <div class="home">
+    <h1>Trade or view your portfolio</h1>
+    <h6>You may Save & Load your data</h6>
+    <h6>Click on 'End Day' to begin a New Day!</h6>
+    <hr />
+    <p>Your Funds: {{ funds }}</p>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  computed: {
+    funds() {
+      return this.$store.getters.funds;
+    },
+  },
+};
 </script>
 
 <style>
+.home {
+  margin-top: 30px;
+}
 </style>

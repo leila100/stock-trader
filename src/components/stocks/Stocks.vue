f529ac5792205b76e2ac4367ba40f4a7d0ac52da
Leila <nb.leila10@gmail.com>
2020-04-09 17:29:16 -0500

    Initialize the stocks in app and connect stocks page to the store stocks

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -8,21 +8,15 @@
 import Stock from "./Stock.vue";
 
 export default {
-  data() {
-    return {
-      stocks: [
-        { id: 1, name: "BMW", price: 110 },
-        { id: 2, name: "Google", price: 200 },
-        { id: 3, name: "Apple", price: 250 },
-        { id: 4, name: "Twitter", price: 8 }
-      ]
-    };
+  computed: {
+    stocks() {
+      return this.$store.getters.stocks;
+    },
   },
   components: {
-    Stock
-  }
+    Stock,
+  },
 };
 </script>
 
-<style>
-</style>
+<style></style>

3eaad8f43ddee68920361b20e0258fb8847d75b9
Leila <nb.leila10@gmail.com>
2020-04-09 16:40:17 -0500

    Add display of stocks as cards in stocks page

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,5 +1,16 @@
 <template>
-  <h1>Stocks - stock component</h1>
+  <div class="col-sm-6 col-md-4">
+    <div class="card border-success">
+      <div class="card-header bg-success">
+        <h4 class="card-title">Name</h4>
+        <div class="card-subtitle">(Price: PRICE)</div>
+      </div>
+      <div class="card-body">
+        <input type="number" class="form-control" placeholder="Quantity" />
+        <a href="#" class="btn btn-success">Buy</a>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,4 +18,18 @@ export default {};
 </script>
 
 <style>
+.card {
+  margin: 15px 0;
+}
+.card-body {
+  display: flex;
+}
+input {
+  margin-right: 20px;
+}
+.card-title,
+.card-subtitle {
+  display: inline;
+  margin: 0 5px;
+}
 </style>
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,9 +1,27 @@
 <template>
-  <h1>Stocks page</h1>
+  <div class="card-deck">
+    <Stock v-for="(stock, index) in stocks" :key="index"></Stock>
+  </div>
 </template>
 
 <script>
-export default {};
+import Stock from "./Stock.vue";
+
+export default {
+  data() {
+    return {
+      stocks: [
+        { id: 1, name: "BMW", price: 110 },
+        { id: 2, name: "Google", price: 200 },
+        { id: 3, name: "Apple", price: 250 },
+        { id: 4, name: "Twitter", price: 8 }
+      ]
+    };
+  },
+  components: {
+    Stock
+  }
+};
 </script>
 
 <style>

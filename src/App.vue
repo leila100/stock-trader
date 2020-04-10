abfc8964d6692be762efc9ef1b8a81440f4b46ea
Leila <nb.leila10@gmail.com>
2020-04-10 18:11:56 -0500

    Add animations to the route transitions

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="container">
     <Header />
-    <router-view />
+    <transition name="slide" mode="out-in">
+      <router-view />
+    </transition>
   </div>
 </template>
 
@@ -9,16 +11,45 @@
 import Header from "./components/Header.vue";
 export default {
   components: {
-    Header,
+    Header
   },
   created() {
     this.$store.dispatch("initStocks");
-  },
+  }
 };
 </script>
 
 <style>
 body {
   padding: 30px;
+}
+
+.slide-enter-active {
+  animation: slide-in 200ms ease-out forwards;
+}
+.slide-leave-active {
+  animation: slide-out 200ms ease-out forwards;
+}
+
+@keyframes slide-in {
+  from {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
+  to {
+    transform: translateY(0);
+    opacity: 1;
+  }
+}
+
+@keyframes slide-out {
+  from {
+    transform: translateY(0);
+    opacity: 1;
+  }
+  to {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
 }
 </style>

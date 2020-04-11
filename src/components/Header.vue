fd2fbf7c19544733fa6d45df789bc7e712226d3c
Leila <nb.leila10@gmail.com>
2020-04-10 19:06:32 -0500

    Add bootstrap-vue to add the dropdown to the header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -14,22 +14,13 @@
       <strong class="navbar-nav mr-auto">Funds: {{ funds | currency }}</strong>
       <ul class="navbar-nav">
         <li class="nav-item">
-          <button class="nav-link" @click="endDay">End Day</button>
+          <a href="#" class="nav-link" @click="endDay">End Day</a>
         </li>
-        <li class="nav-item dropdown">
-          <a
-            class="nav-link dropdown-toggle"
-            href="#"
-            id="navbarDropdown"
-            role="button"
-            data-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-          >Save & Load</a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="#">Save data</a>
-            <a class="dropdown-item" href="#">Load Data</a>
-          </div>
+        <li>
+          <b-dropdown variant="outline-success" id="dropdown-1" text="Save & Load" class="m-md-2">
+            <b-dropdown-item>Save data</b-dropdown-item>
+            <b-dropdown-item>Load Data</b-dropdown-item>
+          </b-dropdown>
         </li>
       </ul>
     </div>
@@ -40,6 +31,11 @@
 import { mapActions } from "vuex";
 
 export default {
+  data() {
+    return {
+      isDropDownOpen: false
+    };
+  },
   computed: {
     funds() {
       return this.$store.getters.funds;
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
 import { routes } from "./routes";
 import store from "./store/store";
@@ -8,6 +12,8 @@ import App from "./App.vue";
 Vue.config.productionTip = false;
 
 Vue.use(VueRouter);
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 Vue.filter("currency", value => {
   return "$" + value.toLocaleString();

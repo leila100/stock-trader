dc73d432f7ee561ce642bc54c90f87bb1fe875f1
Leila <nb.leila10@gmail.com>
2020-04-10 19:22:29 -0500

    Setup vueResource with a root path to a firebase db

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import VueResource from "vue-resource";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
@@ -12,8 +13,11 @@ import App from "./App.vue";
 Vue.config.productionTip = false;
 
 Vue.use(VueRouter);
+Vue.use(VueResource);
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
+
+Vue.http.options.root = "https://stocktrader-18b16.firebaseio.com/";
 
 Vue.filter("currency", value => {
   return "$" + value.toLocaleString();
